9eb8efe7fc44ba67ef0b3586d3dd146bbff3f06e
daplopez <daphnelopez@utexas.edu>
2024-03-05 23:00:52 -0600

    primary itinerary screen mostly done

---
+++ WanderWise/Itinerary/ItineraryTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ItineraryTableViewCell.swift
+//  WanderWise
+//
+//  Created by Daphne Lopez on 3/4/24.
+//
+
+import UIKit
+
+class ItineraryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var notesLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

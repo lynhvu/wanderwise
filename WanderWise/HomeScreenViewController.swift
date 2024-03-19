442a6322c83fb578a8f0218758f42a495807e92b
marianahermidarojas <mariana.02.hr@gmail.com>
2024-03-18 20:50:22 -0500

    Basic UI for homepage

---
+++ WanderWise/HomeScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeScreenViewController.swift
+//  WanderWise
+//
+//  Created by Mariana Hermida Rojas on 3/18/24.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

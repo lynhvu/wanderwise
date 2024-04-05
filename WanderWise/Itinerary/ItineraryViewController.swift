b54399b99652781aff4b5de3dbf9dd75a891fff3
Eduardo Cazares <ecazares31@gmail.com>
2024-04-04 22:57:15 -0500

    Fixed bug where you could not segue from itinerary to edit itinerary. Only thing left is to add functionality to correctly edit trips and their events.

--- WanderWise/Itinerary/ItineraryViewController.swift
+++ WanderWise/Itinerary/ItineraryViewController.swift
@@ -98,14 +98,12 @@ class ItineraryViewController: UIViewController, UITableViewDelegate, UITableVie
         }
     }
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "EditItinerarySegueIdentifier",
+           let destination = segue.destination as? EditItineraryViewController {
+            destination.trip = trip
+        }
     }
-    */
-
 }

dd8e27c544abcf271b555b314071647303d58177
marianahermidarojas <mariana.02.hr@gmail.com>
2024-04-08 23:22:17 -0500

    global map, profile user to first and last name, eddie notif stuff

---
+++ WanderWise/NewTrip/tempController.swift
@@ -0,0 +1,8 @@
+//
+//  tempController.swift
+//  WanderWise
+//
+//  Created by Mariana Hermida Rojas on 4/8/24.
+//
+
+import Foundation
---
+++ WanderWise/User/NotificationService.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationService.swift
+//  WanderWise
+//
+//  Created by Mariana Hermida Rojas on 4/8/24.
+//
+
+import Foundation

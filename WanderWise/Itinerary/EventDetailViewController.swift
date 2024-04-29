90291148b048eaf0b58bf3a01315f087a3434da0
daplopez <daphnelopez@utexas.edu>
2024-04-29 18:03:32 -0500

    added directions to places from event details vc

--- WanderWise/Itinerary/EventDetailViewController.swift
+++ WanderWise/Itinerary/EventDetailViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class EventDetailViewController: UIViewController {
-
+    
     
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
@@ -21,7 +22,7 @@ class EventDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMMM dd, yyyy"
         let timeFormatter = DateFormatter()
@@ -32,5 +33,25 @@ class EventDetailViewController: UIViewController {
         locationLabel.text = selectedEvent.location
         notesLabel.text = selectedEvent.description
     }
+    
+    @IBAction func directionsButtonPressed(_ sender: Any) {
+        openGoogleMapsDirections(placeName: locationLabel.text!)
+    }
+    
+    // Function to open Google Maps with directions given a place's name
+    func openGoogleMapsDirections(placeName: String) {
+        // Construct the URL with the place's name
+        let encodedPlaceName = placeName.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        let urlString = "https://www.google.com/maps/dir/?api=1&destination=\(encodedPlaceName)"
 
+        if let url = URL(string: urlString) {
+            // Check if Google Maps app is installed, if yes, open with directions
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                // If Google Maps app is not installed, open in browser
+                UIApplication.shared.open(URL(string: "https://maps.google.com/?q=\(encodedPlaceName)")!, options: [:], completionHandler: nil)
+            }
+        }
+    }
 }
